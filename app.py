ea8b8f98104f2a61ac71eb40de1cec4d76f59cb1
NakshatraGokule <nakshatragokule@gmail.com>
2026-02-06 20:11:56 +0100

    Initial commit: Spotify project notebook, dataset, and Streamlit app

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Load the CSV
+df = pd.read_csv("SpotifyFeatures.csv")
+
+# Dashboard title
+st.title("Spotify Songs Dashboard")
+
+#  Popularity Distribution
+fig1 = px.histogram(df, x='popularity', nbins=30, title='Track Popularity Distribution')
+st.plotly_chart(fig1)
+
+#  Top 10 Artists by Number of Tracks
+top_artists = df['artist_name'].value_counts().head(10)
+fig2 = px.bar(x=top_artists.values, y=top_artists.index, orientation='h', title='Top 10 Artists by Number of Tracks')
+st.plotly_chart(fig2)
+
+# Danceability vs Popularity
+fig3 = px.scatter(df, x='danceability', y='popularity', title='Danceability vs Popularity')
+st.plotly_chart(fig3)
+
+
